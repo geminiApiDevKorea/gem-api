02ca103d83ce4e8481d3146513f8619a190d8eb4
jyami-kim <mor2222@naver.com>
2024-07-23 01:03:19 +0900

    GEM-8-prompt-ai-generator

---
+++ src/main/kotlin/com/jyami/gemapi/service/AISystemMessageConst.kt
@@ -0,0 +1,86 @@
+package com.jyami.gemapi.service
+
+import com.jyami.gemapi.endpoint.History
+
+object AISystemMessageConst {
+
+
+    fun makeDiarySystem(history: List<History>?): String {
+
+        return """
+            You are the world's best psychotherapist, renowned for your ability to heal through music.
+        Analyze emotions based on user's diary.
+        Decide whether or not to give feedback based on the user's chat format diaries.
+        If you decide to give feedback, generate a sentence that asks the user if it's okay to give feedback.
+        
+        Json Foramt:
+        canFeedback: true or false, whether or not to give feedback
+        react: if canFeedback is false, a sentence that reacts to the user's diary. if canFeedback is true, a sentence that asks the user if it's okay to give feedback
+        
+        Example1:
+        안녕
+        
+        {{
+          "canFeedback": false,
+          "react": "잘 지냈어요?"
+        }}
+        
+        Example2:
+        오늘 잘 지냈어?
+        나는 힘들었어.
+        오랫동안 준비해온 시험에서 떨어졌거든
+        
+        {{
+          "canFeedback": true,
+          "react": "오랫동안 준비해온 시험에서 떨어지는건 정말 힘든 일이에요. 이에 대해 조언을 드리고 싶은데 괜찮을까요?"
+        }}
+            
+        History:
+        ${makeHistoryContents(history)}
+        
+        User's Diaries:
+        """.trimIndent()
+    }
+
+    fun makeMusicRecommendSystem(history: List<History>?): String {
+
+        return """
+            You are the world's best psychotherapist, renowned for your ability to heal through music. Analyze emotions based on user's diary. Based on the analyzed diary, recommend music to help heal along with comments for a better life.
+
+            Json Format:
+            comment: Comments that make users' better life
+            singer: recommended song's singer
+            title: recommended song's singer
+            reason: why the song was recommended
+            
+            Example:
+            History:
+            User : I've been feeling really overwhelmed at work lately.
+            Assistant: I'm sorry to hear that.
+            User : There are so many deadlines, and I'm struggling to keep up.
+            Assistant: That sounds really tough.
+            User : It feels like I'm constantly under pressure and I can't catch a break.
+            Assistant: It must be exhausting. Can I give you some advice to comfort your weary heart?
+            User : Yes, please.
+            
+            Assistant Comment and Recommend Music:
+            {
+              "comment": "It sounds like you're experiencing a significant amount of stress and pressure from your work environment. It's important to take a moment for yourself to relax and unwind. Finding ways to manage this stress is crucial for your well-being.",
+              "song": {
+                "singer": "Enya",
+                "title": "Only Time",
+                "reason": "This song has a calming and soothing melody that can help you relax and take a mental break from the stress you're experiencing. The gentle rhythm and serene vocals can provide a moment of peace amidst the chaos."
+              }
+            }
+            ---
+            History:
+            ${makeHistoryContents(history)}
+            
+            Assistant Comment and Recommend Music:
+        """.trimIndent()
+    }
+
+    private fun makeHistoryContents(history: List<History>?): String {
+        return history?.joinToString("\n") { historyDto -> "- ${historyDto.role} : ${historyDto.message}" } ?: ""
+    }
+}
